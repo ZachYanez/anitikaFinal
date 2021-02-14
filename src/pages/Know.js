8d456583c1fc22c87a87ff57708dc3dbfda7db81
Zach Yanez <zachyanez@gmail.com>
2021-02-14 01:12:08 -0600

    get to know changes

--- src/pages/Know.js
+++ src/pages/Know.js
@@ -3,7 +3,10 @@ import KnowPics from "../components/carousels/knowPics";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Image from "react-bootstrap/Image";
+import Know1 from "../assets/know/Amazon.PNG";
+import Know2 from "../assets/know/Dynamax.PNG";
+import Know3 from "../assets/know/Fogo.PNG";
+import Know4 from "../assets/know/IMG_3869.PNG";
 
 export default function Know() {
   return (
@@ -63,23 +66,38 @@ export default function Know() {
         </Navbar.Collapse>
       </Navbar>
       <div className="content-area background">
+        <br />
+        <div className="textColorWhite ml-5 mr-5 ">
+          Abyon Ausar, a model and actor from Jacksonville, FL. is a multifaceted experiential marketing specialist. His focus is
+          on opportunities in brand management, entertainment, social media marketing, and hosting.
+          <div />
           <br/>
-        <div className="textColorWhite ">
-        Abyon Ausar, a model and actor from Jacksonville, FL. is a multifaceted experiential marketing specialist. His
-          focus is on opportunities in brand management, entertainment, social media marketing, and hosting.
-          <div/>
-          <br />
-          <div>Abyon signed with his first agency in the summer of 2018, and started working professionally in 2019. Since entering
-          the business he's been involved in a variety of commercials, fashion shows, social media campaigns, influencer
-          campaigns, print ads, skits, and independent films. He currently represented for acting and modeling throughout TX, GA,
-          and NY.
+          <div className="mb-5">
+            Abyon signed with his first agency in the summer of 2018, and started working professionally in 2019. Since entering
+            the business he's been involved in a variety of commercials, fashion shows, social media campaigns, influencer
+            campaigns, print ads, skits, and independent films. He currently represented for acting and modeling throughout TX,
+            GA, and NY.
           </div>
         </div>
 
-        <KnowPics />
+        <div>
+          <img className="w-50" src={Know4} alt="slide" />
+          <br/>
+          <img className="w-50 mt-3" src={Know3} alt="slide" />
+        </div>
       </div>
-      <div className="textColorWhite"> Abyon big on character, connection, and experience. His legacy will be in the people he leaves behind. It’s not about what he leaves for himself, it’s what he leaves for others.
-Abyon wants to be remembered for the positive impact he has on those that are close to him. Abyon will be remembered for using his resilience and resourcefulness to defy the odds to better himself and those around him.</div>
+      <br/>
+      <div className="textColorWhite ml-5 mr-5 mb-5">
+        Abyon big on character, connection, and experience. His legacy will be in the people he leaves behind. It’s not about what
+        he leaves for himself, it’s what he leaves for others. Abyon wants to be remembered for the positive impact he has on
+        those that are close to him. Abyon will be remembered for using his resilience and resourcefulness to defy the odds to
+        better himself and those around him.
+      </div>
+      <div className="mx-3">
+        <img className="w-50" src={Know2} alt="slide" />
+        <div className="mt-4"></div>
+        <img className="w-50" src={Know1} alt="slide" />
+      </div>
       <div className="extra-content"></div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="#/">A</Navbar.Brand>
