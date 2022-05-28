8b8acac6bc607339ea20477a0d192a1a14009708
Zach Yanez <zachyanez@gmail.com>
2022-05-28 15:40:04 -0700

    post revert

--- src/components/carousels/fashionPics.js
+++ src/components/carousels/fashionPics.js
@@ -1,25 +1,5 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import fashion1 from "../../assets/fashion/5032_Edit.jpg";
-import fashion2 from "../../assets/fashion/5095_Edit.jpg";
-import fashion3 from "../../assets/fashion/5180_Edit.jpg";
-import fashion4 from "../../assets/fashion/5204_Edit.jpg";
-import fashion5 from "../../assets/fashion/5296_Edit.jpg";
-import fashion6 from "../../assets/fashion/5571_Edit.jpg";
-
-import fashion7 from "../../assets/fashion/IMG_3848.jpg";
-import fashion8 from "../../assets/fashion/IMG_3857.jpg";
-import fashion9 from "../../assets/fashion/IMG_3912.jpg";
-import fashion10 from "../../assets/fashion/IMG_3936.jpg";
-import fashion11 from "../../assets/fashion/IMG_4276.jpg";
-
-import Abyon1 from "../../assets/fashion/Abyon1.jpg";
-import Abyon2 from "../../assets/fashion/Abyon2.jpg";
-import Abyon3 from "../../assets/fashion/Abyon3.jpg";
-import Abyon5 from "../../assets/fashion/Abyon5.jpg";
-import Abyon6 from "../../assets/fashion/Abyon6.jpg";
-import Abyon7 from "../../assets/fashion/Abyon7.jpg";
-import Abyon8 from "../../assets/fashion/Abyon8.jpg";
 
 export default function fashion() {
   return (
